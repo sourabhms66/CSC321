dbbe19b447b2418e326cb7b2878293f709806838
sourabhms66 <sourabhms66@gmail.com>
2023-11-14 23:03:50 +0000

    adding a python program

---
+++ ssamanta11.py
@@ -0,0 +1,9 @@
+# Sourabh Samanta
+# CSC 321
+# Lab 11
+
+# getting an input from the user
+number = float(input("Enter a number: "))
+number = number*number
+# print statement with a saying
+print("Your number squared is: ", number, "Whatever you may do in your life, try to do it well!")
